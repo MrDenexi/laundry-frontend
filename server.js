36b8234c8ca5f28c3065e2c8292806960f50927a
MrDenexi <25987844+MrDenexi@users.noreply.github.com>
2018-07-23 21:27:33 +0200

    finally fixed server.js for real, routing works hooray

--- server.js
+++ server.js
@@ -1,36 +1,48 @@
-var http = require('http');
-var Httpdispatcher = require('httpdispatcher');
-var dispatcher = new Httpdispatcher();
-const port = 8080;
-var static = require('node-static');
-var reactpage = new static.Server('./client/build'); // the react client page
+const fetch = require("node-fetch");
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
 
-function handleRequest(req, res){
-    try{
-        console.log(req.url); //log request
-        dispatcher.dispatch(req, res); //dispatch
-    } catch(err){
-        console.log(err);
-    }
+// API calls
+app.get('/api', (req, res) => {  
+    let wat = req.hostname
+    let wat2 = req.body
+    let wat3 = req.headers
+    let wat4 = req.rawHeaders
+    res.send('<h1> yowwwwwwwwwwwwwwwwwwwwwwwwww api hier </h1>');
+});
+
+// Handle React routing, return all requests to React app
+app.get('/*', function(req, res) {
+    let reqfile = req.path;
+    if (reqfile == '/') reqfile = '/index.html'
+    res.sendFile(path.join(__dirname, 'client/build', reqfile), '', function(err){
+        if (err) {
+            console.log(err);
+            res.sendStatus(404).send();
+        }
+        else console.log('Requested', reqfile)
+    });
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+//---------------------// 
+//----api functions----//
+function getSession(){
+    fetch(
+        'http://cors-anywhere.herokuapp.com/mobile.wgls.laundryrestart.com/api/checkout/start-session',{
+            headers: {'Location': '16'}
+        }
+    ).then(
+        response => {
+            console.log(response)
+            console.log(typeof(response))
+        }
+    ).then(
+        whoopy => {
+            console.log(whoopy)
+            return whoopy
+        }
+    ).catch(err => console.log('There was an error:' + err))
 }
-
-var happyServer = http.createServer(handleRequest);
-
-//fancy routing
-dispatcher.onGet('/api', function(req, res){ // '/api' -> getting the api call.
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1> This is api </h1>')
-})
-dispatcher.onGet('/', function(req,res){ //'/' react page
-    reactpage.serve(req, res);
-})
-dispatcher.onError(function(req, res){ // error stuff
-    res.writeHead(404);
-    res.end("Error, page doesn't exist");
-})
-
-//start happyserver
-happyServer.listen(port, function(){
-    // Hello houston.
-    console.log("Server listening on: http://localhost:%s", port);
-})

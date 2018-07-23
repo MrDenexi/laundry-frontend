00dd967bb2da2998549f2a7b823b59b89ad78faf
MrDenexi <25987844+MrDenexi@users.noreply.github.com>
2018-07-23 18:55:45 +0200

    got server.js finally kinda working yay

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 5000;
+var http = require('http');
+var Httpdispatcher = require('httpdispatcher');
+var dispatcher = new Httpdispatcher();
+const port = 8080;
+var static = require('node-static');
+var reactpage = new static.Server('./client/build'); // the react client page
 
-app.use(express.static(path.join(__dirname, 'client/build'), {etag: false})); 
+function handleRequest(req, res){
+    try{
+        console.log(req.url); //log request
+        dispatcher.dispatch(req, res); //dispatch
+    } catch(err){
+        console.log(err);
+    }
+}
 
-// use strict routing
-app.enable('strict routing');
+var happyServer = http.createServer(handleRequest);
 
-// API calls
-app.get('/api', (req, res) => {  
-    let wat = req.host
-    let wat2 = req.body
-    let wat3 = req.headers
-    let wat4 = req.rawHeaders
-    res.send(console.log('zien we dit yo?'));
-});
+//fancy routing
+dispatcher.onGet('/api', function(req, res){ // '/api' -> getting the api call.
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1> This is api </h1>')
+})
+dispatcher.onGet('/', function(req,res){ //'/' react page
+    reactpage.serve(req, res);
+})
+dispatcher.onError(function(req, res){ // error stuff
+    res.writeHead(404);
+    res.end("Error, page doesn't exist");
+})
 
-// Handle React routing, return all requests to React app
-app.get('/waku', function(req, res) {  
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-app.listen(port, () => console.log(`Listening on port ${port}`));
+//start happyserver
+happyServer.listen(port, function(){
+    // Hello houston.
+    console.log("Server listening on: http://localhost:%s", port);
+})
